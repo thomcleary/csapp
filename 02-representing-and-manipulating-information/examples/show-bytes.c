1fbabd215fb04fc25dcf7f88e934e0d0d48ddf70
thomcleary <thomasclearydev@gmail.com>
2024-11-12 15:47:23 +0800

    Chapter 2: Problem 29

--- 02-representing-and-manipulating-information/examples/show-bytes.c
+++ 02-representing-and-manipulating-information/examples/show-bytes.c
@@ -1,9 +1,10 @@
+#include <stddef.h>
 #include <stdio.h>
 
 typedef unsigned char *byte_pointer;
 
 void show_bytes(byte_pointer start, size_t len) {
-  int i;
+  size_t i;
   for (i = 0; i < len; i++) {
     printf(" %.2x", start[i]);
   }
